d69ed25c649dc70074df2936c42f79705f57993b
Tammy Dresen <dre15003@byui.edu>
2018-05-07 15:59:14 -0700

    cart

--- web/shoppingCart/templates/head.php
+++ web/shoppingCart/templates/head.php
@@ -1,3 +1,3 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="cart.css" type="text/css" rel="stylesheet">
+<link href="../cart.css" type="text/css" rel="stylesheet">
--- web/week3.php
+++ web/week3.php
@@ -2,10 +2,22 @@
 <!-- Tammy Dresen, COMM310, -->
 <html lang="en-us">
   <head>
-    <title>Home page</title>
+    <title>Week 3 form page</title>
     <?php include 'template/head.php'?>
 
 </head>
   <body>
+    <form action="info.php" method="post">
+      Name: <input type="text" name="name"<br>
+      Email: <input type="text" name="email"<br>
+      Major:<br>
+<input type="radio" name="major" value="cs">Computer Science<br>
+<input type="radio" name="major" value="wdd">Web Design and Development<br>
+<input type="radio" name="major" value="cit">Computer Information Technology<br>
+<input type="radio" name="major" value="ce">Computer Engineering<br>
+Comments: <textarea
+
+
+    </form>
   </body>
   </html>
