43274bbe5dbf091759df1249589ed6e4dacfd3ad
Tammy Dresen <dre15003@byui.edu>
2018-05-07 15:46:32 -0700

    shopping cart

---
+++ web/shoppingCart/templates/footer.php
@@ -0,0 +1,8 @@
+<footer><p>Copyright Tammy Dresen 2018</p>
+
+</footer>
+
+
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="js/site.js"></script>
---
+++ web/week3.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!-- Tammy Dresen, COMM310, -->
+<html lang="en-us">
+  <head>
+    <title>Home page</title>
+    <?php include 'template/head.php'?>
+
+</head>
+  <body>
+  </body>
+  </html>
