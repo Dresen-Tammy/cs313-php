3c81aed98b8f36c9635eb5f7aa947c74713da89b
Tammy Dresen <dre15003@byui.edu>
2018-05-07 16:26:58 -0700

    cart

---
+++ web/shoppingCart/view/browseItems.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!-- Tammy Dresen, CS313,
+List of items to add to cart and purchase.
+Button or link to add item to cart
+Clicking button will store item in session, return to browseItem page  -->
+<html lang="en-us">
+  <head>
+    <title>Browse</title>
+    <?php include 'template/head.php'?>
+
+</head>
+  <body>
+
+      <header>
+        <div class="logo">
+          <div class=" bigCircle"></div>
+          <div class=" smallCircle"></div>
+          <div class="tinyCircle"></div>
+          <h1>Company Name</h1>
+        </div>
+        <a  href="home.php" class="button1">CART</a>
+      </header>
+      <main>
+        <h2>Item 1</h2><input type="button" value=""
+    </main>
+
+
+    </form>
+  </body>
+  </html>
