328d3755b13d429e39d9e52f0e36fef57ea4e549
Tammy Dresen <dre15003@byui.edu>
2018-05-08 17:24:44 -0700

    shopping cart

--- web/shoppingCart/index.php
+++ web/shoppingCart/index.php
@@ -23,6 +23,27 @@ if($action == NULL) {
 
 switch ($action) {
   case 'browse':
+      // list of items for sale on browseItems page
+      $items = array
+      (
+        "RU" => array("Specialized Ruby Bike 52cm Teal", 1299.99),
+        "RO" => array("Specialized Roubaix Bike 61cm Mint", 3099.00 ),
+        "CH" => array("Trek Checkpoint ALR Bike 48cm Blue", 1789.99),
+        "PR" => array("Giant Propel Advanced Bike 59cm Red", 3775.00 ),
+        "LA" => array("Lazer 02 Helmet Orange Sm", 89.96 ),
+        "PI" => array("Pearl Izumi Cycling Kit Sm", 89.95 ),
+        "SI" => array("Sidi Level Carbon Shoes Mens 8", 119.99 ),
+        "PR"=> array("Giant Propel Advanced 59cm Red", 3775.00 ),
+        "BG" => array("Specialized Body Geometry Gel Gloves Lg", 35.00 )
+      );
+      // create browse list for browseItem page
+      $browseList = "<ul>";
+      foreach ($items as $key => $value) {
+        $browseList += "<li>$value[0], $value[1]<button name='browse' value='$key'>Add To Cart</button><br>"
+      }
+      $browseList += "</ul>"
+
+
       include 'view/browseItems.php';
       break;
   case 'cart':
--- web/shoppingCart/view/browseItems.php
+++ web/shoppingCart/view/browseItems.php
@@ -16,22 +16,15 @@ Clicking button will store item in session, return to browseItem page  -->
           <div class=" bigCircle"></div>
           <div class=" smallCircle"></div>
           <div class="tinyCircle"></div>
-          <h1>Company Name</h1>
+          <h1>Recyclery</h1>
         </div>
         <a  href="home.php" class="button1">CART</a>
       </header>
       <main>
-        <h2>Item 1</h2><input type="button" value="i1" name="Add To Cart">
-        <h2>Item 2</h2><input type="button" value="i2">
-        <h2>Item 3</h2><input type="button" value="i3">
-        <h2>Item 4</h2><input type="button" value="i4">
-        <h2>Item 5</h2><input type="button" value="i5">
-        <h2>Item 6</h2><input type="button" value="i6">
-        <h2>Item 7</h2><input type="button" value="i7">
-        <h2>Item 8</h2><input type="button" value="i8">
-        <h2>Item 9</h2><input type="button" value="i9">
-        <h2>Item 10</h2><input type="button" value="i10">
-    </main>
+        <?php
+        echo $browseList;
+        ?>
+        </main>
 
 
     </form>
