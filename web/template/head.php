53ece9f563503ae306d54d2fdb0b0edc26869173
Tammy Dresen <31862261+Dresen-Tammy@users.noreply.github.com>
2018-05-10 00:51:08 -0700

    Add files via upload

--- web/template/head.php
+++ web/template/head.php
@@ -1,3 +1,3 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="/web/css/site.css" type="text/css" rel="stylesheet">
+<link href="css/site.css" type="text/css" rel="stylesheet">
